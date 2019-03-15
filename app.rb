7381aac8c01c88635ed4859508a459bbcd5e50ce
Josh <joshsimon85@yahoo.com>
2019-03-15 16:14:22 -0600

    Recap

--- app.rb
+++ app.rb
@@ -132,6 +132,7 @@ end
 Recaptcha.configure do |config|
   config.site_key  = '6Le7oRETAAAAAETt105rjswZ15EuVJiF7BxPROkY'
   config.secret_key = '6Le7oRETAAAAAL5a8yOmEdmDi3b2pH7mq5iH1bYK'
+  config.proxy = 'https://www.frankenkopter.com'
 end
 
 include Recaptcha::ClientHelper
